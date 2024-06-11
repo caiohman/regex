2e86b439ace97cfd222696adf12342d9fe8457fd
caiohman <cbalthazar70@gmail.com>
2024-06-11 07:00:55 -0300

    add *+? and response

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // Start by typing OK in the Regex field to proceed to the first step and access the more detailed description..
 const ok = "Understand? OK or NOT";
 console.log(ok.match(/OK/g));
-
+// response [ 'OK' ]
 
 //Return all .pdf files
 const pdf = "readme.md \
@@ -12,27 +12,45 @@ image.png \
 music.mp4 \
 manual.pdf";
 console.log(pdf.match(/\w+\.pdf/gm));
+//response 'document.pdf', 'manual.pdf'
 
 //All chars
 const anyChar = "abcABC123 .:!?";
 console.log(anyChar.match(/./g));
+//response  'a', 'b', 'c', 'A','B', 'C', '1', '2','3', ' ', '.', ':','!', '?'
 
 const bar = "bar ber bir bor bur";
 console.log(bar.match(/b[aeiou]r/g));
+//response  'bar', 'ber', 'bir', 'bor', 'bur'
 
 // Letter Range[a-z]
 // To find the letters in the specified range, the starting letter and the ending letter are written in square brackets [] with a dash between them -. 
 // It is case-sensitive. Type the expression that will select all lowercase letters between e and o, including themselves.
 const string = "abcdefghijklmnopqrstuvwxyz";
 console.log(string.match(/[e-o]/g));
+//response 'e', 'f', 'g', 'h','i', 'j', 'k', 'l','m', 'n', 'o'
 
 const numbers = "0123456789";
 console.log(numbers.match(/[3-6]/g));
-
+//response  '3', '4', '5', '6'
 
 //It will negate beor and beur
-const negateSpecificWord = "bear beor beer beur"
+const negateSpecificWord = "bear beor beer beur";
 console.log(negateSpecificWord.match(/be[^ou]r/g));
+//response 'bear', 'beer'
 
+
+//Matching words with e char that doesn't happen or happens 1+ times 
+const asterisk = "br ber beer";
+console.log(asterisk.match(/be*r/g));
+//response  'br', 'ber', 'beer'
+
+const plus = "br ber beer";
+console.log(plus.match(/be+r/g));
+//response 'ber', 'beer'
+
+const optional = "color, colour";
+console.log(optional.match(/colou?r/g));
+//response 'color', 'colour'
 
 
